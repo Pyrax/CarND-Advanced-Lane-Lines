ed5fb353ebbc412e5ddbf92c8e187bdb7bd8425a
Pyrax <piranhadev@gmail.com>
2018-08-30 21:22:17 +0200

    Add debug information to evaluate

--- src/LaneDetector.py
+++ src/LaneDetector.py
@@ -8,35 +8,31 @@ class Lane:
     def __init__(self):
         self.detected = False   # was the line detected in the last iteration?
 
-        self.best_x = None                              # average x values of the fitted line over the last n iterations
-        self.recent_x_fitted = []                       # x values of the last n fits of the line
-        self.best_fit = None                            # polynomial coefficients averaged over the last n iterations
-        self.current_fit = [np.array([False])]          # polynomial coefficients for the most recent fit
-        self.current_fit_x = None                       # points for plotting polynomial
-        self.diffs = np.array([0,0,0], dtype='float')   # difference in fit coefficients between last and new fits
+        self.current_fit = [np.array([False])]  # polynomial coefficients for the most recent fit
+        self.current_fit_x = None               # points for plotting polynomial
 
-        self.radius_of_curvature = None     # radius of curvature of the line in some units
+        self.radius_of_curvature = None     # radius of curvature of the line in meters
         self.line_base_pos = None           # distance in meters of vehicle center from the line
 
         self.all_x = None   # x values for detected line pixels
         self.all_y = None   # y values for detected line pixels
 
-    def update(self, line_x, line_y, fit, fit_x, plot_y):
+    def update(self, line_x, line_y, fit, fit_x, curvature, distance_from_center):
+        self.detected = True
+
         self.all_x = line_x
         self.all_y = line_y
 
-        if fit is None:
-            self.detected = False
-            self.current_fit = [np.array([False])]
-        else:
-            self.current_fit = fit
-
+        self.current_fit = fit
         self.current_fit_x = fit_x
+
+        self.radius_of_curvature = curvature
+        self.line_base_pos = distance_from_center
 
     def get_pixels(self):
         return self.all_x, self.all_y
 
-    def get_coefficients(self):
+    def get_fit(self):
         return self.current_fit
 
     def get_fit_x(self):
@@ -65,27 +61,25 @@ class LaneDetector:
         self.plot_y = np.linspace(0, self.image.shape[0] - 1, self.image.shape[0])
 
         strategy = self.determine_strategy()
+        self.last_used_strategy = strategy
         left_x, left_y, right_x, right_y = strategy.find_lane(self.image,
                                                               self.left_lane.current_fit,
                                                               self.right_lane.current_fit)
         left_fit, right_fit, left_fit_x, right_fit_x = self.fit_poly(left_x, left_y, right_x, right_y)
 
-        self.left_lane.update(left_x, left_y, left_fit, left_fit_x, self.plot_y)
-        self.right_lane.update(right_x, right_y, right_fit, right_fit_x, self.plot_y)
+        left_curverad, right_curverad = self.metrics.measure_curvature_real(left_fit, right_fit, self.plot_y)
+        left_lane_pos = self.metrics.measure_lane_position(image, left_fit, self.plot_y)
+        right_lane_pos = self.metrics.measure_lane_position(image, right_fit, self.plot_y)
+
+        self.left_lane.update(left_x, left_y, left_fit, left_fit_x, left_curverad, left_lane_pos)
+        self.right_lane.update(right_x, right_y, right_fit, right_fit_x, right_curverad, right_lane_pos)
 
     def determine_strategy(self):
-        default_strategy = self.poly_search
+        for lane in [self.left_lane, self.right_lane]:
+            if not lane.detected:
+                return self.sliding_window
 
-        def find_other_strategy_by_lane_data(lanes):
-            for lane in lanes:
-                if not lane.detected:
-                    return self.sliding_window
-
-        better_strategy = find_other_strategy_by_lane_data([self.left_lane, self.right_lane])
-
-        strategy = default_strategy if not better_strategy else better_strategy
-        self.last_used_strategy = strategy
-        return strategy
+        return self.poly_search
 
     def get_visualization(self):
         return self.last_used_strategy.visualize_lanes(self.image,
@@ -120,18 +114,14 @@ class LaneDetector:
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         # Display the curvature
-        left_curverad, right_curverad = self.metrics.measure_curvature_real(self.left_lane.get_coefficients(),
-                                                                            self.right_lane.get_coefficients(),
-                                                                            self.plot_y)
+        left_curverad, right_curverad = self.left_lane.radius_of_curvature, self.right_lane.radius_of_curvature
         cv2.putText(display_image, f'Left curvature radius: {left_curverad:.4f}m', (40, 60),
                     font, 1, (255, 255, 255), 2, cv2.LINE_AA)
         cv2.putText(display_image, f'Right curvature radius: {right_curverad:.4f}m', (40, 100),
                     font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Display car's position relative to the center
-        center_dist = self.metrics.measure_car_position(self.image,
-                                                        self.left_lane.get_coefficients(),
-                                                        self.right_lane.get_coefficients(),
+        center_dist = self.metrics.measure_car_position(self.image, self.left_lane.get_fit(), self.right_lane.get_fit(),
                                                         self.plot_y)
         cv2.putText(display_image, f'Position to center: {center_dist:.4f}m', (40, 140), font, 1, (255, 255, 255),
                     2, cv2.LINE_AA)
@@ -153,3 +143,20 @@ class LaneDetector:
             right_fit_x = 1 * self.plot_y ** 2 + 1 * self.plot_y
 
         return left_fit, right_fit, left_fit_x, right_fit_x
+
+    def display_debug_info(self, display_image):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        def display_lane_debug_info(lane, name, x_pos, y_off):
+            cv2.putText(display_image, name, (x_pos, y_off + 60), font, 1, (255, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(display_image, f'Curvature: {lane.radius_of_curvature:.2f}m', (x_pos, y_off + 100),
+                        font, 1, (255, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(display_image, f'Distance from center: {lane.line_base_pos:.4f}m', (x_pos, y_off + 140),
+                        font, 1, (255, 255, 0), 2, cv2.LINE_AA)
+
+            return display_image
+
+        for lane in [[self.left_lane, 'Left lane', 40, 0], [self.right_lane, 'Right lane', 40, 260]]:
+            display_lane_debug_info(lane[0], lane[1], lane[2], lane[3])
+
+        return display_image
